05991116d039c3446022fb80d6e3f2d658cf88da
Derek2195-git <derek.garzon@uabc.edu.mx>
2026-09-03 23:49:44 -0700

    Añadidos algunos archivos principales
    Se añadio la clase ManejoRecursividad para los ejercicios y Main para probar los metodos de ManejoRecursividad

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public static void main(String[] args) {
+    ManejoRecursividad recursividad = new ManejoRecursividad();
+
+
+    int conejos =10;
+
+    System.out.println("Tengo 10 conejos, y estos tienen " + recursividad.contarOrejas(conejos) + " orejas.");
+
+}
